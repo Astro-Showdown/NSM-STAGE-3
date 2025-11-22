f1951f71894f8dc031343f2aaa81de83b271b403
Astro-Showdown <astro.showdown@gmail.com>
2025-11-22 16:49:15 +0800

    Update findbutton.js

--- findbutton.js
+++ findbutton.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $("img").on("click", function(event) {
+$(Starmap.html.ready(function() {
+    $("StarMap").on("click", function(event) {
         var x = event.pageX - this.offsetLeft;
         var y = event.pageY - this.offsetTop;
         alert("X Coordinate: " + x + " Y Coordinate: " + y);
